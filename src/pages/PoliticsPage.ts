51538a418e89f5b383ea1a68ee008851fdfa925c
pawanuk <pawanmantra@gmail.com>
2024-08-24 03:52:21 +0100

    Refactored and cleaned up code for place bets feature

--- src/pages/PoliticsPage.ts
+++ src/pages/PoliticsPage.ts
@@ -1,86 +1,113 @@
 import { BasePage } from './BasePage';
+import { BetResult } from '../types/BetResult';
+import { Locator } from '@playwright/test'; 
 
 export class PoliticsPage extends BasePage {
-  async navigateToPoliticsSection() {
-    console.log("Attempting to click the Politics tab...");
-    const locators = [
-      this.page.locator('#subnav').getByRole('link', { name: 'Politics' }),
-      this.page.locator('.subnav-link.mod-link').filter({ hasText: 'Politics' }),
-      this.page.locator('a[data-event-type-name="politics"]'),
-      this.page.locator('a[href="en/politics-betting-2378961"]'),
-      this.page.locator('a:has-text("Politics")')
-    ];
-    let elementFound = false;
-    for (const locator of locators) {
-      try {
-        const element = await locator.first();
-        if (await element.isVisible()) {
-          console.log('Politics link found and visible.');
-          await element.click();
-          elementFound = true;
-          break;
-        }
-      } catch (error) {
-        console.log('Locator failed, trying next one...');
-        continue;
-      }
-    }
-    if (!elementFound) {
-      console.log('Politics link not found or not clickable, navigating directly to the URL...');
-      await this.goto('https://www.betfair.com/exchange/plus/en/politics-betting-2378961', { waitUntil: 'networkidle' });
-    } else {
-      console.log("Waiting for the Politics page to load...");
-      await this.page.waitForLoadState('networkidle', { timeout: 20000 });
-    }
-    console.log("Politics page loaded.");
-  }
 
-  async placeBet(candidateName: string, odds: number, amount: number) {
-    console.log(`Adding bet for: ${candidateName} with odds ${odds} and amount ${amount}`);
-    const candidateRow = this.page.locator(`//h3[text()="${candidateName}"]/ancestor::tr`);
-    await candidateRow.waitFor();
-    const backButton = candidateRow.locator('.bet-buttons.back-cell.last-back-cell button:has-text("£")');
-    await backButton.click();
-    await this.page.waitForSelector('betslip-editable-bet');
-    const betslip = this.page.locator('betslip-editable-bet').filter({ hasText: `${candidateName} £` });
-    const textBoxLocator = betslip.locator('betslip-price-ladder').getByRole('textbox');
-    const sizeInputLocator = betslip.locator('betslip-size-input').getByRole('textbox');
-    await textBoxLocator.fill(odds.toString());
-    await sizeInputLocator.fill(amount.toString());
-    console.log(`Bet added to betslip for ${candidateName}.`);
-  }
+  private loc_politicsLink = this.page.locator('#subnav').getByRole('link', { name: 'Politics' });
+  private loc_politicsAltLinks = [
+    this.page.locator('.subnav-link.mod-link').filter({ hasText: 'Politics' }),
+    this.page.locator('a[data-event-type-name="politics"]'),
+    this.page.locator('a[href="en/politics-betting-2378961"]'),
+    this.page.locator('a:has-text("Politics")')
+  ];
 
-  async getDisplayedProfit(candidateName: string, betIndex: number): Promise<number> {
-        const profitLocator = this.page.locator(`//span[text()="${candidateName}"]/ancestor::div/following-sibling::div//span[contains(text(),'£')]`);
+  private locators = {
+    betslip: this.page.locator('betslip-editable-bet'),
+    profitLocator: (candidateName: string) => this.page.locator(`//span[text()="${candidateName}"]/ancestor::div/following-sibling::div//span[contains(text(),'£')]`),
+    loc_placeBetButton: this.page.locator("//ours-button[contains(.,'Place bets')]"),
+    betslipOdds: (betslip: Locator) => betslip.locator('betslip-price-ladder').getByRole('textbox'),
+    betslipAmount: (betslip: Locator) => betslip.locator('betslip-size-input').getByRole('textbox'),
+    logoutButton: this.page.getByRole('button', { name: 'Log Out' }),
+    myAccountButton: this.page.getByText('My Account pawanuk My Betfair'),
+  };
 
-    const profitText = await profitLocator.textContent();
-    console.log(`Using locator for profit: ${profitLocator}`);
-    return parseFloat(profitText!.replace(/[^0-9.-]+/g, ""));
-  }
+  async navigateToPoliticsSection(): Promise<void> {
+    console.log("Attempting to click the Politics tab...");
+    let elementFound = false;
 
-  async logout() {
-    console.log("Logging out...");
-    await this.page.getByText('My Account pawanuk My Betfair').click();
-    const locators = [
-      this.page.getByRole('button', { name: 'Log Out' }),
-      this.page.locator('button:has-text("Log Out")')
-    ];
-    let logoutButton;
-    for (const locator of locators) {
-      try {
-        logoutButton = await locator.first();
-        if (await logoutButton.isVisible()) {
-          console.log('Log Out button found and visible.');
-          await logoutButton.click();
-          break;
-        }
-      } catch (error) {
-        console.log('Locator failed, trying next one');
-        continue;
-      }
-    }
-    if (!logoutButton) {
-      throw new Error('Log Out button not found using any of the locators');
-    }
-  }
+    for (const locator of [this.loc_politicsLink, ...this.loc_politicsAltLinks]) {
+      try {
+        const element = await locator.first();
+        if (await element.isVisible()) {
+          console.log('Politics link found and visible.');
+          await element.click();
+          elementFound = true;
+          break;
+        }
+      } catch (error) {
+        console.log('Locator failed, trying next one...');
+        continue;
+      }
+    }
+
+    if (!elementFound) {
+      console.log('Politics link not found or not clickable, navigating directly to the URL...');
+      await this.goto('https://www.betfair.com/exchange/plus/en/politics-betting-2378961', { waitUntil: 'networkidle' });
+    } else {
+      console.log("Waiting for the Politics page to load...");
+      await this.page.waitForLoadState('networkidle', { timeout: 20000 });
+    }
+
+    console.log("Politics page loaded.");
+  }
+
+  async addBetToBetslip(candidateName: string, odds: number, amount: number): Promise<void> {
+    console.log(`Adding bet for: ${candidateName} with odds ${odds} and amount ${amount}`);
+    const candidateRow = this.page.locator(`//h3[text()="${candidateName}"]/ancestor::tr`);
+    await candidateRow.waitFor();
+
+    // Click the last back bet button in the candidate's row
+    const backButton = candidateRow.locator('.bet-buttons.back-cell.last-back-cell button:has-text("£")');
+    await backButton.click();
+    console.log(`Clicked back bet button for candidate: ${candidateName}`);
+
+    // Wait for the betslip to be visible
+    await this.page.waitForSelector('betslip-editable-bet');
+
+    // Locate the corresponding betslip entry
+    const betslip = this.locators.betslip.filter({ hasText: `${candidateName} £` });
+    
+    // Fill in the odds and stake in the betslip
+    await this.locators.betslipOdds(betslip).fill(odds.toString());
+    await this.locators.betslipAmount(betslip).fill(amount.toString());
+
+    console.log(`Bet added to betslip for ${candidateName} with odds ${odds} and stake ${amount}`);
+  }
+
+  async getDisplayedProfit(candidateName: string): Promise<number> {
+    const profitLocator = this.locators.profitLocator(candidateName);
+    const profitText = await profitLocator.textContent();
+
+    if (profitText === null) {
+      throw new Error(`Profit text for ${candidateName} was not found.`);
+    }
+
+    console.log(`Using locator for profit: ${profitLocator}`);
+    return parseFloat(profitText.replace(/[^0-9.-]+/g, ""));
+  }
+
+  async logout(): Promise<void> {
+    console.log("Logging out...");
+    await this.locators.myAccountButton.click();
+
+    const locators = [this.locators.logoutButton, this.page.locator('button:has-text("Log Out")')];
+    let logoutButton;
+    for (const locator of locators) {
+      try {
+        logoutButton = await locator.first();
+        if (await logoutButton.isVisible()) {
+          console.log('Log Out button found and visible.');
+          await logoutButton.click();
+          break;
+        }
+      } catch (error) {
+        console.log('Locator failed, trying next one');
+        continue;
+      }
+    }
+    if (!logoutButton) {
+      throw new Error('Log Out button not found using any of the locators');
+    }
+  }
 }
--- src/test/steps/place_bets_steps.ts
+++ src/test/steps/place_bets_steps.ts
@@ -5,133 +5,132 @@ import { LoginPage } from '../../pages/LoginPage';
 import { PoliticsPage } from '../../pages/PoliticsPage';
 import fs from 'fs-extra';
 import path from 'path';
-type BetResult = {
-    name: string;
-    odds: number;
-    amount: number;
-    profit: number;
-  };
-  
-  let browser: Browser;
-  let context: BrowserContext;
-  let page: Page;
-  let loginPage: LoginPage;
-  let politicsPage: PoliticsPage;
-  
-  Before(async function () {
-    console.log("Preparing test environment...");
-    
-    await cleanDirectories();  // Clean screenshots and videos directories
-    
-    browser = await chromium.launch({
-      headless: process.env.DOCKER_ENV === 'true',  
-    });
-  
-    context = await browser.newContext({
-      storageState: 'auth.json',
-      recordVideo: { dir: 'videos/', size: { width: 1280, height: 1024 } }
-    });
-  
-    page = await context.newPage();
-    loginPage = new LoginPage(page);
-    politicsPage = new PoliticsPage(page);
-    console.log("Browser launched and new page created.");
-  });
-  
-  After(async function (scenario) {
-    console.log("Tearing down...");
-    await takeScreenshotOnFailure(scenario); // Take screenshot if scenario fails
-    await closeResources(); // Close page, context, and browser
-  });
-  
-  Given('I am logged in to Betfair', { timeout: 60 * 1000 }, async function () {
-    await loginPage.login(ENV.username!, ENV.password!);
-  });
-  
-  When('I navigate to the Politics section', { timeout: 20 * 1000 }, async function () {
-    await politicsPage.navigateToPoliticsSection();
-  });
-  
-  When('I place a bet on the following candidates:', { timeout: 120 * 1000 }, async function (dataTable) {
-    const candidates = dataTable.hashes();
-    const expectedResults: BetResult[] = [];
-  
-    for (const candidate of candidates) {
-      const randomOdds = Math.floor(Math.random() * (5 - 2 + 1)) + 2;
-      const randomAmount = Math.floor(Math.random() * (500 - 10 + 1)) + 10;
-      const expectedProfit = (randomOdds - 1) * randomAmount;
-  
-      expectedResults.push({ name: candidate.candidate, odds: randomOdds, amount: randomAmount, profit: expectedProfit });
-  
-      await politicsPage.placeBet(candidate.candidate, randomOdds, randomAmount);
-    }
-  
-  
-  
-    for (let i = 0; i < expectedResults.length; i++) {
-      const candidate = expectedResults[i];
-      const actualProfit = await politicsPage.getDisplayedProfit(candidate.name, i + 1);
-      console.log(`Verifying bet for ${candidate.name}`);
-  
-      if (candidate.profit !== actualProfit) {
-        throw new Error(`Verification failed for ${candidate.name}: Expected profit: ${candidate.profit}, Actual profit: ${actualProfit}`);
-      }
-    }
-  });
-  
-  Then('I log out from the application', async function () {
-    await politicsPage.logout();
-  });
-  
-  // Utility functions
-  
-  async function cleanDirectories() {
-    const videoDir = path.join(__dirname, '../../videos');
-    const screenshotDir = path.join(__dirname, '../../screenshots');
-  
-    try {
-      console.log("Deleting old videos and screenshots...");
-      if (fs.existsSync(videoDir)) {
-        await fs.remove(videoDir);
-        console.log(`Deleted video directory: ${videoDir}`);
-      }
-      if (fs.existsSync(screenshotDir)) {
-        await fs.remove(screenshotDir);
-        console.log(`Deleted screenshot directory: ${screenshotDir}`);
-      }
-    } catch (err) {
-      console.error("Error cleaning up directories:", err);
-    }
-  }
-  
-  async function takeScreenshotOnFailure(scenario: any) {
-    if (scenario.result?.status === Status.FAILED && page) {
-      const screenshotPath = path.join('screenshots', `${scenario.pickle.name}.png`);
-      console.log(`Scenario failed. Taking screenshot: ${screenshotPath}`);
-      await page.screenshot({ path: screenshotPath, fullPage: true });
-    }
-  }
-  
-  async function closeResources() {
-    if (page && !page.isClosed()) {
-      console.log("Closing page...");
-      await page.close();
-    }
-  
-    if (context) {
-      console.log("Closing context...");
-      await context.close();
-    }
-  
-    if (browser) {
-      console.log("Closing browser...");
-      await browser.close();
-    }
-  
-    console.log("Teardown completed.");
-  }
-  
-  
 
-  
-  
+type BetResult = {
+  name: string;
+  odds: number;
+  amount: number;
+  profit: number;
+};
+
+let browser: Browser;
+let context: BrowserContext;
+let page: Page;
+let loginPage: LoginPage;
+let politicsPage: PoliticsPage;
+
+Before(async function () {
+  console.log("Preparing test environment...");
+  await cleanDirectories(); // Clean screenshots and videos directories
+
+  browser = await chromium.launch({
+    headless: process.env.DOCKER_ENV === 'true',
+  });
+
+  context = await browser.newContext({
+    storageState: 'auth.json',
+    recordVideo: { dir: 'videos/', size: { width: 1280, height: 1024 } }
+  });
+
+  page = await context.newPage();
+  loginPage = new LoginPage(page);
+  politicsPage = new PoliticsPage(page);
+  console.log("Browser launched and new page created.");
+});
+
+After(async function (scenario) {
+  console.log("Tearing down...");
+  await takeScreenshotOnFailure(scenario); // Take screenshot if scenario fails
+  await closeResources(); // Close page, context, and browser
+});
+
+Given('I am logged in to Betfair', { timeout: 60 * 1000 }, async function () {
+  await loginPage.login(ENV.username!, ENV.password!);
+});
+
+When('I navigate to the Politics section', { timeout: 20 * 1000 }, async function () {
+  await politicsPage.navigateToPoliticsSection();
+});
+
+When('I place a bet on the following candidates:', { timeout: 120 * 1000 }, async function (dataTable) {
+  const candidates = dataTable.hashes();
+  const expectedResults: BetResult[] = [];
+
+  for (const candidate of candidates) {
+    const randomOdds = Math.floor(Math.random() * (5 - 2 + 1)) + 2;
+    const randomAmount = Math.floor(Math.random() * (500 - 10 + 1)) + 10;
+    const expectedProfit = (randomOdds - 1) * randomAmount;
+
+    expectedResults.push({
+      name: candidate.candidate,
+      odds: randomOdds,
+      amount: randomAmount,
+      profit: expectedProfit,
+    });
+
+    await politicsPage.addBetToBetslip(candidate.candidate, randomOdds, randomAmount);
+  }
+
+  // Verification step
+  for (let i = 0; i < expectedResults.length; i++) {
+    const candidate = expectedResults[i];
+    const actualProfit = await politicsPage.getDisplayedProfit(candidate.name);
+    console.log(`Verifying bet for ${candidate.name}`);
+
+    if (candidate.profit !== actualProfit) {
+      throw new Error(`Verification failed for ${candidate.name}: Expected profit: ${candidate.profit}, Actual profit: ${actualProfit}`);
+    }
+  }
+});
+
+Then('I log out from the application', async function () {
+  await politicsPage.logout();
+});
+
+// Utility functions
+
+async function cleanDirectories() {
+  const videoDir = path.join(__dirname, '../../videos');
+  const screenshotDir = path.join(__dirname, '../../screenshots');
+
+  try {
+    console.log("Deleting old videos and screenshots...");
+    if (fs.existsSync(videoDir)) {
+      await fs.remove(videoDir);
+      console.log(`Deleted video directory: ${videoDir}`);
+    }
+    if (fs.existsSync(screenshotDir)) {
+      await fs.remove(screenshotDir);
+      console.log(`Deleted screenshot directory: ${screenshotDir}`);
+    }
+  } catch (err) {
+    console.error("Error cleaning up directories:", err);
+  }
+}
+
+async function takeScreenshotOnFailure(scenario: any) {
+  if (scenario.result?.status === Status.FAILED && page) {
+    const screenshotPath = path.join('screenshots', `${scenario.pickle.name}.png`);
+    console.log(`Scenario failed. Taking screenshot: ${screenshotPath}`);
+    await page.screenshot({ path: screenshotPath, fullPage: true });
+  }
+}
+
+async function closeResources() {
+  if (page && !page.isClosed()) {
+    console.log("Closing page...");
+    await page.close();
+  }
+
+  if (context) {
+    console.log("Closing context...");
+    await context.close();
+  }
+
+  if (browser) {
+    console.log("Closing browser...");
+    await browser.close();
+  }
+
+  console.log("Teardown completed.");
+}
