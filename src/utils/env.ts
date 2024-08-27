e63139180a3a725f70cec7629f564dd4b47900e5
pawanuk <pawanmantra@gmail.com>
2024-08-27 14:05:08 +0100

    minor changes

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,13 @@
+// import * as dotenv from 'dotenv';
+
+// dotenv.config();
+
+// export const ENV = {
+//   username: process.env.BETFAIR_USERNAME!,
+//   password: process.env.BETFAIR_PASSWORD!,
+//   url: process.env.BETFAIR_URL!,
+// };
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,4 +16,7 @@ export const ENV = {
   username: process.env.BETFAIR_USERNAME!,
   password: process.env.BETFAIR_PASSWORD!,
   url: process.env.BETFAIR_URL!,
+  pageLoadTimeout: parseInt(process.env.PAGE_LOAD_TIMEOUT || '10000', 10),
+  inputDelay: parseInt(process.env.INPUT_DELAY || '3000', 10),
+  elementTimeout: parseInt(process.env.ELEMENT_TIMEOUT || '60000', 10),
 };
